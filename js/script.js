02336cf6e36016d3cf956f5802968ad82c12db25
AFIF HEREA MILPARIAN <simillbrian@gmail.com>
2024-06-30 21:31:07 +0700

    feat: form

--- js/script.js
+++ js/script.js
@@ -13,3 +13,33 @@ window.addEventListener("load", function() {
       document.querySelector(".name").textContent = userName;
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    const namaDisplay = document.getElementById('nama');
+    const tanggalLahirDisplay = document.getElementById('tanggalLahir');
+    const jenisKelaminDisplay = document.getElementById('gender');
+    const pesanDisplay = document.getElementById('pesan');
+    const currentTimeDisplay = document.getElementById('time');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Mencegah form dari mengirimkan secara default
+
+        // Mengambil data dari form
+        const nama = document.getElementById('nama').value;
+        const tanggalLahir = document.getElementById('tanggalLahir').value;
+        const jenisKelamin = document.querySelector('input[name="gender"]:checked').value;
+        const pesan = document.querySelector('.pesan').value;
+
+        // Menampilkan data ke elemen yang sesuai
+        namaDisplay.textContent = nama;
+        tanggalLahirDisplay.textContent = tanggalLahir;
+        jenisKelaminDisplay.textContent = jenisKelamin;
+        pesanDisplay.textContent = pesan;
+
+        // Menampilkan waktu saat ini
+        const now = new Date();
+        currentTimeDisplay.textContent = now.toLocaleTimeString();
+    });
+});
+
